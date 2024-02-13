361fc9eb9e481c6e5f37001c5d76670e4630cb10
Rapa285 <prmahesa2004@gmail.com>
2024-02-13 12:04:15 +0700

    add : error handling read file

--- src/baca_input.py
+++ src/baca_input.py
@@ -16,3 +16,40 @@ def baca_file(x):
         seq_reward[i] = int(file.readline())
     return buffer_size,matrix,row,col,seq,seq_reward
 
+def check_file_validity(file):
+    valid = True
+    f = open(file,"r")
+    buffer_size = int(f.readline())
+    if buffer_size < 1:
+        print("Buffer size invalid")
+        print("Buffer size minimum : 1")
+        valid = False
+
+
+    r2 = f.readline().split()
+    row = int(r2[0])
+    col = int(r2[1])
+
+    if row < 1 or col < 1:
+        print("Row or Column size invalid")
+        print("Row and Column size minimum : 1")
+        valid = False
+
+    matrix = ["" for i in range(row)]
+    for i in range(row):
+        matrix[i] = f.readline().split()
+
+
+    number_of_seq = int(f.readline())
+    if number_of_seq < 1:
+        print("Row or Column size invalid")
+        print("Row and Column size minimum : 1")
+        valid = False
+        
+    seq = ["" for i in range(number_of_seq)]
+    seq_reward = [0 for i in range(number_of_seq)]
+    for i in range(number_of_seq):
+        seq[i] = f.readline().split()
+        seq_reward[i] = int(f.readline())
+    return valid
+
